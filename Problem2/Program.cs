06ccd536eba43a30ea3fea6919c1c8aa65f111f8
cherooooo <101722878+cherooooo@users.noreply.github.com>
2022-03-16 21:35:31 +0800

    Add files via upload
    Initial commit

---
+++ Problem2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace Problem2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
+            StringBuilder sb = new StringBuilder();
+            sb.Append(number);
+            string filePath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            string pathWithFileName = Path.Combine(filePath, "log.txt");
+            bool exists = Directory.Exists(filePath);
+            if (!exists)
+                Directory.CreateDirectory(filePath);
+            using (StreamWriter stream = File.AppendText(pathWithFileName))
+            {
+                stream.Write(sb.ToString());
+            }
+            sb.Clear();
+            string values = File.ReadAllText(pathWithFileName).ToString();
+            Console.WriteLine("Number in log.txt: {0}", values);
+        }
+    }
+}
